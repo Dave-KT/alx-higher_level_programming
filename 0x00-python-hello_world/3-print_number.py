25e2df58136685c3392ac92e9795e3235d87b15b
Dave-KT <daveafro45@gmail.com>
2022-08-02 11:15:46 +0300

    Python

--- 0x00-python-hello_world/3-print_number.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-number = 98
-if (isinstance(number, int)):
-    print(f"{number} Battery street")
-else:
-    print("ValueError: Unknown format code 'd' for object of type 'str'")
--- 0x00-python-hello_world/4-print_float.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-number = 3.14159
-if (isinstance(number, float)):
-    print(f"Float: {number:.2f}")
-else:
-    print("ValueError: Unknown format code 'd' for object of type 'str'")
